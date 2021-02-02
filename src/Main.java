fb2a87fba2ea66543fb5be443c62a4f391ed7bdb
Josh Codd <jjc21@live.co.uk>
2021-02-02 23:13:29 +0000

    Tidy up.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import javafx.application.Application;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-import models.CTHeadViewer;
-import models.Volume;
 import views.Menu;
 import views.Viewer;
 
@@ -11,12 +7,8 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) {
-
         Menu menu = new Menu(stage);
-        Volume v = new Volume(256,256, 113);
-
         new Viewer(stage, menu.getRoot());
-
     }
 
     public static void main(String[] args) {
