b0a4998a84c9b9697589d8b54d00f34f5bbe1a80
Josh Codd <jjc21@live.co.uk>
2021-02-04 18:22:14 +0000

    Add javadoc.

--- src/controllers/ViewerController.java
+++ src/controllers/ViewerController.java
@@ -9,6 +9,11 @@ import javafx.stage.Stage;
 import models.CTHeadViewer;
 import views.Menu;
 
+/**
+ * Controller class for the viewer interface.
+ * Handles all user input and displays scans.
+ * @author Josh Codd.
+ */
 public class ViewerController {
     @FXML
     public ImageView firstView;
@@ -36,7 +41,10 @@ public class ViewerController {
     WritableImage front_image;
     WritableImage side_image;
 
-    public void initialize() {
+    /**
+     * Initialises UI elements to be ready for display.
+     */
+    public void init() {
         top_image = new WritableImage(ctHead.getTop_width(), ctHead.getTop_height());
         front_image = new WritableImage(ctHead.getFront_width(), ctHead.getFront_height());
         side_image = new WritableImage(ctHead.getSide_width(), ctHead.getSide_height());
@@ -71,18 +79,18 @@ public class ViewerController {
         });
 
         firstViewSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-                ctHead.drawSlice(top_image, newValue.intValue(), "top");
-                sliderValueStyle(firstViewSlider);
+            ctHead.drawSlice(top_image, newValue.intValue(), "top");
+            sliderValueStyle(firstViewSlider);
         });
 
         secondViewSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-                ctHead.drawSlice(front_image, newValue.intValue(), "front");
-                sliderValueStyle(secondViewSlider);
+            ctHead.drawSlice(front_image, newValue.intValue(), "front");
+            sliderValueStyle(secondViewSlider);
         });
 
         thirdViewSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-                    ctHead.drawSlice(side_image, newValue.intValue(), "side");
-                    sliderValueStyle(thirdViewSlider);
+            ctHead.drawSlice(side_image, newValue.intValue(), "side");
+            sliderValueStyle(thirdViewSlider);
         });
 
         opacitySlider.valueProperty().addListener((observable, oldValue, newValue) -> {
@@ -112,6 +120,10 @@ public class ViewerController {
         openFileButton.setOnAction(e -> menu.getRoot().setVisible(!menu.getRoot().isVisible()));
     }
 
+    /**
+     * Changes the style of a slider to be a different colour up to the value selected/thumb location.
+     * @param slider The slider to update.
+     */
     public void sliderValueStyle(Slider slider){
         double value = (slider.getValue() - slider.getMin()) / (slider.getMax() - slider.getMin()) * 100.0 ;
         slider.lookup(".slider .track").setStyle(String.format("-fx-background-color: " +
@@ -119,20 +131,42 @@ public class ViewerController {
                 value, value));
     }
 
+    /**
+     * Sets the viewer to use.
+     * @param ctHead The viewer to use.
+     */
     public void setCTHeadViewer (CTHeadViewer ctHead) {
         this.ctHead = ctHead;
     }
 
+    /**
+     * Gets the slider that sets opacity.
+     * @return The opacity slider.
+     */
     public Slider getOpacitySlider(){
         return opacitySlider;
     }
 
+    /**
+     * Gets the button that displays middle slide.
+     * @return The mid slide button.
+     */
     public Button getMidSlideButton(){
         return midSlideButton;
     }
 
-    public StackPane getMenuPane() { return menuPane; }
+    /**
+     * Gets the pane in which the menu is to be displayed..
+     * @return The menu pane.
+     */
+    public StackPane getMenuPane() {
+        return menuPane;
+    }
 
+    /**
+     * Sets the stage of the application.
+     * @param stage The stage to set.
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
