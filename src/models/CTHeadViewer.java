f11119d613ecf8f79f1e7a0c019a2f2f7b7aba0d
Josh Codd <jjc21@live.co.uk>
2021-02-11 17:01:14 +0000

    Refactor

--- src/models/CTHeadViewer.java
+++ src/models/CTHeadViewer.java
@@ -2,7 +2,6 @@ package models;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
-import sun.lwawt.macosx.CSystemTray;
 
 /**
  * Represents a ct head scan viewer and the algorithms that can be carried out on them.
@@ -18,6 +17,7 @@ public class CTHeadViewer {
     private final double LIGHT_SOURCE_Y = 20;
     private final double LIGHT_SOURCE_Z = 256;
     private final Volume ctHead;
+
     private double opacity = 0.12;
     private boolean isGradient = false;
     private boolean isGradientInterpolation = false;
@@ -35,6 +35,24 @@ public class CTHeadViewer {
         this.FRONT_HEIGHT = volume.getCT_z_axis();
         this.SIDE_WIDTH = volume.getCT_x_axis();
         this.SIDE_HEIGHT = volume.getCT_z_axis();
+    }
+
+    /**
+     *Gets the correct voxel for the direction/view you want to see.
+     * @param view The view/direction to display e.g top, side or front
+     * @param i The x value to get.
+     * @param j The y value to get.
+     * @param k The z value to get.
+     * @return The correct voxel.
+     */
+    public short getVoxel(String view, int i, int j, int k) {
+        if (view.equals("top")) {
+            return ctHead.getVoxel(k, j, i);
+        } else if (view.equals("side")) {
+            return ctHead.getVoxel(j, i, k);
+        } else {
+            return ctHead.getVoxel(j, k, i);
+        }
     }
 
     /**
@@ -59,37 +77,100 @@ public class CTHeadViewer {
     }
 
     /**
-     *Gets the correct voxel for the direction/view you want to see.
-     * @param view The view/direction to display e.g top, side or front
-     * @param i The x value to get.
-     * @param j The y value to get.
-     * @param k The z value to get.
-     * @return The correct voxel.
+     * Performs volume rendering on the specified image/scan.
+     * @param image The image to write to.
+     * @param view The direction to view the scan/dataset from. i.e front, side or top.
      */
-    public short getVoxel(String view, int i, int j, int k) {
-        if (view.equals("top")) {
-            return ctHead.getVoxel(k, j, i);
-        } else if (view.equals("side")) {
-            return ctHead.getVoxel(j, i, k);
-        } else {
-            return ctHead.getVoxel(j, k, i);
-        }
+    public void volumeRender(WritableImage image, String view) {
+        int w = (int) image.getWidth(), h = (int) image.getHeight();
+        PixelWriter image_writer = image.getPixelWriter();
+        int rayLength = (view.equals("top")) ? ctHead.getCT_z_axis() : ctHead.getCT_x_axis();
+
+        for (int j = 0; j < h; j++) {
+            for (int i = 0; i < w; i++) {
+                double alphaAccum = 1;
+                double redAccum = 0;
+                double greenAccum = 0;
+                double blueAccum = 0;
+                boolean hitBone = false;
+                double L = 1;
+
+                for (int ray = 0; ray < rayLength && !hitBone; ray++) {
+                    short currentVoxel = getVoxel(view, i, j, ray);
+                    if (currentVoxel >= 400 && isGradient) {
+                        Vector surfaceNormal;
+                        L = getDiffuseLighting(i,j,ray,view,currentVoxel,rayLength,w,h);
+                        hitBone = true;
+                    }
+
+                    if (!isGradient || hitBone) {
+                        double[] colour = transferFunction(currentVoxel);
+                        double sigma = colour[3];
+                        redAccum = Math.min(redAccum + (alphaAccum * sigma * L * colour[0]), 1);
+                        greenAccum = Math.min(greenAccum + (alphaAccum * sigma * L * colour[1]), 1);
+                        blueAccum = Math.min(blueAccum + (alphaAccum * sigma * L * colour[2]), 1);
+                        alphaAccum = alphaAccum * (1 - sigma);
+                    }
+                }
+                image_writer.setColor(i, j, Color.color(redAccum, greenAccum, blueAccum, 1));
+            }//column
+        }//row
     }
 
     /**
      * Calculates the lighting/shading of a pixel.
-     * @param y The y axis location of the pixel.
-     * @param x The x axis location of the pixel.
+     * @param i The y axis location of the pixel.
+     * @param j The x axis location of the pixel.
      * @param ray The z or ray depth location of the pixel.
      * @param surfaceNormal The surface normal of the data.
      * @return The lighting value for said pixel.
      */
-    public double getLighting(int x, int y, double ray, Vector surfaceNormal) {
+    public double getDiffuseLighting(int i, int j, int ray, String view, int currentVoxel, int rayLength, int w, int h) {
+        Vector surfaceNormal = getSurfaceNormal(i, j, ray, rayLength, view, w, h);
+        Vector intersection = new Vector(i,j,ray);
+
+        if (isGradientInterpolation && currentVoxel != 400) {
+            int prevRay = (ray > 0) ? (ray - 1) : ray;
+            short prevVoxel = getVoxel(view, i, j, prevRay);
+            double actualIntersection =
+                    linearInterpolationPosition(400, prevVoxel, currentVoxel, prevRay, ray);
+            surfaceNormal = getSurfaceNormal(i, j, actualIntersection, rayLength, view, w, h);
+            intersection.setC(actualIntersection);
+        }
+
         Vector lightSourcePosition = new Vector(lightSourceX, LIGHT_SOURCE_Y, LIGHT_SOURCE_Z);
-        Vector lightDirection = lightSourcePosition.subtract(new Vector(x, y, ray));
+        Vector lightDirection = lightSourcePosition.subtract(intersection);
         lightDirection.normalize();
         surfaceNormal.normalize();
         return Math.max(0, surfaceNormal.dotProduct(lightDirection));
+    }
+
+    public double getGradient(double x,int i1, int j1, int k1, int i2, int j2, int k2, String view, int min, int max, int i) {
+        if (i > min && i < (max - 1)) {
+            double x1 = getVoxel(view, i1, j1, k1);
+            double x2 = getVoxel(view, i2, j2, k2);
+            return x2 - x1;
+        } else if (i <= min) {
+            double x2 = getVoxel(view, i2, j2, k2);
+            return x2 - x;
+        } else {
+            double x1 = getVoxel(view, i1, j1, k1);
+            return x - x1;
+        }
+    }
+
+    public double getGradient(double x,int i1, int j1, double k1, int i2, int j2, double k2, String view, int min, int max, double i) {
+        if (i > min && i < (max - 1)) {
+            double x1 = getRealVoxel(view, i1, j1, k1);
+            double x2 = getRealVoxel(view, i2, j2, k2);
+            return x2 - x1;
+        } else if (i <= min) {
+            double x2 = getRealVoxel(view, i2, j2, k2);
+            return x2 - x;
+        } else {
+            double x1 = getRealVoxel(view, i1, j1, k1);
+            return x - x1;
+        }
     }
 
     /**
@@ -104,41 +185,9 @@ public class CTHeadViewer {
     public Vector getSurfaceNormal(int i, int j, int ray, int rayLength, String view, int w, int h) {
         double x, y, z;
         short currentVoxel = getVoxel(view, i, j, ray);
-        if (i > 0 && i < (w - 1)) {
-            double x1 = getVoxel(view, i - 1, j, ray);
-            double x2 = getVoxel(view, i + 1, j, ray);
-            x = x2 - x1;
-        } else if (i <= 0) {
-            double x2 = getVoxel(view, i + 1, j, ray);
-            x = x2 - currentVoxel;
-        } else {
-            double x1 = getVoxel(view, i - 1, j, ray);
-            x = currentVoxel - x1;
-        }
-
-        if (j > 0 && j < (h - 1)) {
-            double y1 = getVoxel(view, i, j - 1, ray);
-            double y2 = getVoxel(view, i, j + 1, ray);
-            y = y2 - y1;
-        } else if (j <= 0) {
-            double y2 = getVoxel(view, i, j + 1, ray);
-            y = y2 - currentVoxel;
-        } else {
-            double y1 = getVoxel(view, i, j - 1, ray);
-            y = currentVoxel - y1;
-        }
-
-        if (ray > 0 && ray < (rayLength - 1)) {
-            double z1 = getVoxel(view, i, j, ray - 1);
-            double z2 = getVoxel(view, i, j, ray + 1);
-            z = z2 - z1;
-        } else if (ray <= 0) {
-            double z2 = getVoxel(view, i, j, ray + 1);
-            z = z2 - currentVoxel;
-        } else {
-            double z1 = getVoxel(view, i, j, ray - 1);
-            z = currentVoxel - z1;
-        }
+        x = getGradient(currentVoxel,i-1,j,ray,i+1,j,ray,view,0,(w-1),i);
+        y = getGradient(currentVoxel,i,j-1,ray,i,j+1,ray,view,0,h-1,j);
+        z = getGradient(currentVoxel,i,j,ray-1,i,j,ray+1,view,0,rayLength-1,ray);
         return new Vector(x, y, z);
     }
 
@@ -154,42 +203,9 @@ public class CTHeadViewer {
     public Vector getSurfaceNormal(int i, int j, double ray, int rayLength, String view, int w, int h) {
         double x, y, z;
         double currentVoxel = getRealVoxel(view, i, j, ray);
-
-        if (i > 0 && i < (w - 1)) {
-            double x1 = getRealVoxel(view, i - 1, j, ray);
-            double x2 = getRealVoxel(view, i + 1, j, ray);
-            x = x2 - x1;
-        } else if (i <= 0) {
-            double x2 = getRealVoxel(view, i + 1, j, ray);
-            x = x2 - currentVoxel;
-        } else {
-            double x1 = getRealVoxel(view, i - 1, j, ray);
-            x = currentVoxel - x1;
-        }
-
-        if (j > 0 && j < (h - 1)) {
-            double y1 = getRealVoxel(view, i, j - 1, ray);
-            double y2 = getRealVoxel(view, i, j + 1, ray);
-            y = y2 - y1;
-        } else if (j <= 0) {
-            double y2 = getRealVoxel(view, i, j + 1, ray);
-            y = y2 -currentVoxel;
-        } else {
-            double y1 = getRealVoxel(view, i, j - 1, ray);
-            y = currentVoxel - y1;
-        }
-
-        if (ray >= 1 && ray < (rayLength - 2)) {
-            double z1 = getRealVoxel(view, i, j, ray - 1.0);
-            double z2 = getRealVoxel(view, i, j, ray + 1.0);
-            z = z2 - z1;
-        } else if (ray <= 1) {
-            double z2 = getRealVoxel(view, i, j, ray + 1.0);
-            z = z2 - currentVoxel;
-        } else {
-            double z1 = getRealVoxel(view, i, j, ray - 1.0);
-            z = currentVoxel - z1;
-        }
+        x = getGradient(currentVoxel,i-1,j,ray,i+1,j,ray,view,0,(w-1),i);
+        y = getGradient(currentVoxel,i,j-1,ray,i,j+1,ray,view,0,h-1,j);
+        z = getGradient(currentVoxel,i,j,ray-1,i,j,ray+1,view,1,rayLength-2,ray);
         return new Vector(x, y, z);
     }
 
@@ -218,11 +234,8 @@ public class CTHeadViewer {
      * @param x2 The integer position after.
      * @return The voxel value at non integer position X.
      */
-    public double linearInterpolationVoxel(double v1, double v2, double x1, double x, int x2) {
-        double dividend = x - x1;
-        double divisor = x2 - x1;
-        double quotient = dividend / divisor;
-        return (v1 + (v2 - v1) * quotient);
+    public double linearInterpolationVoxel(double v1, double v2, double x1, double x, double x2) {
+        return (v1 + (v2 - v1) * ((x - x1)/(x2 - x1)));
     }
 
     /**
@@ -234,70 +247,8 @@ public class CTHeadViewer {
      * @param x2 The following integer position.
      * @return The non integer position of the specified value.
      */
-    public double linearInterpolationPosition(int v, int v1, int v2, int x1, int x2) {
-        double dividend = v - v1;
-        double divisor = v2 - v1;
-        double quotient = dividend / divisor;
-        return x1 + (x2 - x1) * quotient;
-    }
-
-    /**
-     * Performs volume rendering on the specified image/scan.
-     * @param image The image to write to.
-     * @param view The direction to view the scan/dataset from. i.e front, side or top.
-     */
-    public void volumeRender(WritableImage image, String view) {
-        int w = (int) image.getWidth(), h = (int) image.getHeight();
-        PixelWriter image_writer = image.getPixelWriter();
-        int rayLength = (view.equals("top")) ? ctHead.getCT_z_axis() : ctHead.getCT_x_axis();
-
-        for (int j = 0; j < h; j++) {
-            for (int i = 0; i < w; i++) {
-                double alphaAccum = 1;
-                double redAccum = 0;
-                double greenAccum = 0;
-                double blueAccum = 0;
-                boolean hitBone = false;
-                double L = 1;
-
-                for (int ray = 0; ray < rayLength && !hitBone; ray++) {
-                    short currentVoxel = getVoxel(view, i, j, ray);
-                    if (currentVoxel >= 400 && isGradient) {
-                        Vector surfaceNormal;
-                        if (isGradientInterpolation) {
-                            double actualIntersection;
-                            int prevRay = (ray > 0) ? (ray - 1) : ray;
-                            short prevVoxel = getVoxel(view, i, j, prevRay);
-
-                            if (currentVoxel == 400){
-                                surfaceNormal = getSurfaceNormal(i, j, ray, rayLength, view, w, h);
-                                L = getLighting(i, j, ray, surfaceNormal);
-                            } else {
-                                actualIntersection =
-                                        linearInterpolationPosition(400, prevVoxel, currentVoxel, prevRay, ray);
-                                surfaceNormal = getSurfaceNormal(i, j, actualIntersection, rayLength, view, w, h);
-                                L = getLighting(i, j, actualIntersection, surfaceNormal);
-                            }
-
-                        } else {
-                            surfaceNormal = getSurfaceNormal(i, j, ray, rayLength, view, w, h);
-                            L = getLighting(i, j, ray, surfaceNormal) ;
-                        }
-                        hitBone = true;
-                    }
-
-                    if (!isGradient || hitBone) {
-                        double[] colour = transferFunction(currentVoxel);
-                        double sigma = colour[3];
-                        redAccum = Math.min(redAccum + (alphaAccum * sigma * L * colour[0]), 1);
-                        greenAccum = Math.min(greenAccum + (alphaAccum * sigma * L * colour[1]), 1);
-                        blueAccum = Math.min(blueAccum + (alphaAccum * sigma * L * colour[2]), 1);
-                        alphaAccum = alphaAccum * (1 - sigma);
-                    }
-                }
-                image_writer.setColor(i, j, Color.color(redAccum, greenAccum, blueAccum, 1));
-            }//column
-        }//row
+    public double linearInterpolationPosition(double v, double v1, double v2, int x1, int x2) {
+        return x1 + (x2 - x1) * ((v - v1)/(v2 - v1));
     }
 
     /**
