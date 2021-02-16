fb9087ade05356d1a430ee4657142f55a15e77e9
Josh Codd <jjc21@live.co.uk>
2021-02-16 23:59:14 +0000

    Add javadoc.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,25 @@ import javafx.stage.Stage;
 import views.Menu;
 import views.Viewer;
 
+/**
+ * Main class of the CT viewer. Responsible for starting the application.
+ */
 public class Main extends Application{
 
+    /**
+     * JavaFX start method.
+     * @param stage The stage to display on.
+     */
     @Override
     public void start(Stage stage) {
         Menu menu = new Menu(stage);
         new Viewer(stage, menu.getRoot());
     }
 
+    /**
+     * Runs the application.
+     * @param args Command line arguments Not used in this application.
+     */
     public static void main(String[] args) {
         launch(args);
     }
